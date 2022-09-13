9ab3b8e677b267b9c7f14804af988305df99dd5f
Bianca Laura <biancalaura.rosa@gmail.com>
2022-09-13 11:25:03 -0300

    curso de nest

---
+++ src/tweets/tweets-count/tweets-count.service.ts
@@ -0,0 +1,36 @@
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Interval } from '@nestjs/schedule';
+import { InjectModel } from '@nestjs/sequelize';
+import { Tweet } from '../entities/tweet.entity';
+import { Cache } from 'cache-manager';
+
+@Injectable()
+export class TweetsCountService {
+  private limit = 10;
+  constructor(
+    @InjectModel(Tweet)
+    private tweetModel: typeof Tweet,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
+  ) {}
+
+  @Interval(5000) //tasks, cronjob
+  async countTweets() {
+    console.log('Procurando Tweets');
+    let offset = await this.cacheManager.get<number>('tweet-offset');
+    offset = offset === undefined ? 0 : offset;
+
+    console.log(`offset: $${offset}`);
+    const tweets = await this.tweetModel.findAll({
+      offset,
+      limit: this.limit,
+    });
+    console.log(`${tweets.length} encontrados`);
+    if (tweets.length === this.limit) {
+      this.cacheManager.set('tweet-offset', offset + this.limit, {
+        ttl: 1 * 60 * 10,
+      });
+      console.log(`Achou + ${this.limit} Tweets`);
+    }
+  }
+}
